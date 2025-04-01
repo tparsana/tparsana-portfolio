e5b275cb1615e532868abdfa8a8c4413e9ce1db9
Tanish Parsana <116770988+tparsana@users.noreply.github.com>
2025-03-31 21:33:15 -0700

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,9 +9,9 @@ export const siteConfig = {
   urls: {
     github: "https://github.com/tparsana",
     linkedin: "https://www.linkedin.com/in/tanish-parsana/",
-    twitter: "https://twitter.com",
+    twitter: "https://x.com/ParsanaTanish",
   },
   // Replace with your actual Web3Forms access key
   // Sign up at https://web3forms.com/
-  web3FormsKey: "YOUR_WEB3FORMS_ACCESS_KEY",
+  web3FormsKey: "09f94264-14da-46f5-8d59-1d327212f104",
 };

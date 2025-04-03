a52bdf07d88dd6636a9ae574d04568f3ac6eb765
Tanish Parsana <116770988+tparsana@users.noreply.github.com>
2025-04-03 01:55:55 -0700

    Update AnimatedBackground.tsx

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -30,7 +30,7 @@ const AnimatedBackground = () => {
       timestamp = time;
       
       // Increased speed by multiplying time by 1.8
-      const speedFactor = 3.5;
+      const speedFactor = 10;
       
       setBlobPositions(prev => ({
         blob1: {
